48ae904a7ac8d8f6229b2756a8d1df0448f4dc70
carla <cfpcarla@gmail.com>
2020-08-27 21:04:22 -0400

    Implement style and change components

--- my-app/src/components/Persona.js
+++ my-app/src/components/Persona.js
@@ -41,10 +41,11 @@ const useStyles = makeStyles((theme) => ({
     marginTop: theme.spacing(2),
     padding: theme.spacing(2),
     margin: "auto",
-    maxWidth: "80%",
+    maxWidth: "95%",
   },
   image: {
-    height: 256,
+    height: 300,
+    width: 200,
   },
 }));
 
@@ -63,12 +64,12 @@ const Persona = (props) => {
           <Grid item xs={2}>
             <img
               className={classes.image}
-              src="https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+              src="https://images.unsplash.com/photo-1581065178047-8ee15951ede6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
             ></img>
           </Grid>
           <Grid item xs={10}>
-            <Typography variant="body2" color="textSecondary" component="p">
-              This is Emmily, she loves to study, read books and go out with her
+            <Typography variant="h4" color="textSecondary" component="h4">
+              This is Emily, she loves to study, read books and go out with her
               friends. She has a degree in business and worked in a company in
               Toronto, but due to the economic crisis of many companies because
               of COVID 19, Anna lost her job, and now she is looking for a
@@ -78,32 +79,36 @@ const Persona = (props) => {
         </Grid>
       </Paper>
 
-      <Grid item xs={3} sm={3}>
-        <Card className={classes.root}>
-          <CardMedia
-            className={classes.media}
-            image="https://images.unsplash.com/photo-1589386417686-0d34b5903d23?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-          />
-          <CardContent>
-            <Typography variant="body2" color="textSecondary" component="p">
+      <Paper className={classes.paper}>
+        <Grid container spacing={3}>
+          <Grid item xs={2}>
+            <img
+              className={classes.image}
+              src="https://images.unsplash.com/photo-1596496555044-14173b55edee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+            ></img>
+          </Grid>
+          <Grid item xs={10}>
+            <Typography variant="h4" color="textSecondary" component="h4">
               This is Jack he loves to play video games with friends and play
               tennis. He has just graduated in computer science at Geoge Brown
               in Toronto and is looking for a first job opportunity, but due to
               Covid19 he was having a hard time finding a job and also needs to
               continue studying so as not to forget what he learned at college.
             </Typography>
-          </CardContent>
-        </Card>
-      </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
 
-      <Grid item xs={3} sm={3}>
-        <Card className={classes.root}>
-          <CardMedia
-            className={classes.media}
-            image="https://images.unsplash.com/photo-1573497019940-1c28c88b4f3e?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-          />
-          <CardContent>
-            <Typography variant="body2" color="textSecondary" component="p">
+      <Paper className={classes.paper}>
+        <Grid container spacing={3}>
+          <Grid item xs={2}>
+            <img
+              className={classes.image}
+              src="https://images.unsplash.com/photo-1573497019940-1c28c88b4f3e?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+            ></img>
+          </Grid>
+          <Grid item xs={10}>
+            <Typography variant="h4" color="textSecondary" component="h4">
               This is Anna, she is married and has 2 children, she loves
               spending time with her family. She has worked for many years in a
               company and has now decided to change jobs and careers. But for
@@ -111,11 +116,18 @@ const Persona = (props) => {
               she has opportunities for what she is looking for she also needs
               to update her resume and cover letter.
             </Typography>
-          </CardContent>
-        </Card>
-      </Grid>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => displayService()}
+            >
+              Learn more
+            </Button>
+          </Grid>
+        </Grid>
+      </Paper>
 
-      <Grid item xs={3} sm={3} className={classes.secondGrid}>
+      {/* <Grid item xs={3} sm={3} className={classes.secondGrid}>
         <Typography paragraph>
           Are you like Emmily, Jack or Anna also lost your job and or can't get
           a job during the covid? Do you feel that your experience is not enough
@@ -126,16 +138,16 @@ const Persona = (props) => {
             Glaukopis Wisdome can help you with that. Our platform has a
             personalized service for those looking for a job. Click in the
             button to find out more about our services.
-          </Typography>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => displayService()}
-          >
-            Click here
-          </Button>
-        </CardContent>
-      </Grid>
+          </Typography> */}
+      {/* <Button
+        variant="contained"
+        color="primary"
+        onClick={() => displayService()}
+      >
+        Click here
+      </Button> */}
+      {/* </CardContent>
+      </Grid> */}
     </>
   );
 };
